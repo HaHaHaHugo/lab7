d1dd566618fd029e4a97239ef356c649a5c234fe
HaHaHaHugo <Navapol_naksen@cmu.ac.th>
2023-12-08 17:09:01 +0700

    lab7_3.cpp completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,19 +1,13 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
-}
-
-int main(){
-	//Test Case
-	cout << before('A') << "\n";
-	cout << before('B') << "\n";
-	cout << before('P') << "\n";
-	cout << before('T') << "\n";
-	cout << before('Z') << "\n";
-	cout << before('a') << "\n";
-	cout << before('0') << "\n";
-	cout << before('c') << "\n";
-	return 0;
+    if(x>'@'&& x<'['){
+       if(x=='A'){
+          return 'Z';
+       }
+	return x-1;
+	}
+	return '0';
 }
